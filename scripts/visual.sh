0315f97b909ac1f49d8ea7df7270269cfce2bb7b
lkeab <keleiwhu@gmail.com>
2022-04-14 22:13:04 +0200

    add demo imgs

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -2,6 +2,6 @@ export PYTHONPATH=$PYTHONPATH:`pwd`
 # export CUDA_LAUNCH_BLOCKING=1 # for debug
 
 CUDA_VISIBLE_DEVICES=0 python3 demo/demo.py --config-file configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x_4gpu_transfiner.yaml \
-  --input 'datasets/coco/val2017/*.jpg' \
-  --output 'vis_coco_r50/' \
+  --input 'demo/sample_imgs/*.jpg' \
+  --output 'vis_coco_r50_sample/' \
   --opts MODEL.WEIGHTS ./pretrained_model/output_3x_transfiner_r50.pth
