21d7f9c15b6c02a0a1c08ef2adee82161246ac7f
lkeab <keleiwhu@gmail.com>
2022-04-14 15:52:39 +0200

    update vis script

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH=$PYTHONPATH:`pwd`
 # export CUDA_LAUNCH_BLOCKING=1 # for debug
 
-CUDA_VISIBLE_DEVICES=0 python3 demo/demo.py --config-file configs/COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x_4gpu_transfiner_deform.yaml \
+CUDA_VISIBLE_DEVICES=0 python3 demo/demo.py --config-file configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x_4gpu_transfiner.yaml \
   --input 'datasets/coco/val2017/*.jpg' \
-  --output 'vis_coco_ours_new1/' \
-  --opts MODEL.WEIGHTS ./pretrained_model/output_3x_finetune_r101_deform.pth
+  --output 'vis_coco_r50/' \
+  --opts MODEL.WEIGHTS ./pretrained_model/output_3x_transfiner_r50.pth
